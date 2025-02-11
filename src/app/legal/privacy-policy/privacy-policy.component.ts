e33c465c7684c9c250d1ee2dda64a8fafa3bbe4f
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-02-11 16:41:56 +0100

    4th commit

--- src/app/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/legal/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-privacy-policy',
-  standalone: true,
-  imports: [TranslatePipe],
-  templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss',
+	selector: 'app-privacy-policy',
+	standalone: true,
+	imports: [TranslatePipe, CommonModule],
+	templateUrl: './privacy-policy.component.html',
+	styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {}
