a949e548f0465670c80512814051c0a486dc3dfa
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-02-21 14:23:29 +0100

    Mobile Menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { TranslationService } from '../../core/services/translation.service';
 import { TranslatePipe } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
@@ -11,14 +11,36 @@ import { CommonModule } from '@angular/common';
 	styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-	constructor(private translationService: TranslationService) {}
-
+	constructor(
+		private translationService: TranslationService,
+		private renderer: Renderer2, // Renderer2 für DOM-Manipulation
+	) {}
+	isMenuOpen = false;
 	currentLang: string = localStorage.getItem('language') || 'en';
 
 	toggleLanguage(lang: string) {
 		if (this.currentLang !== lang) {
 			this.translationService.toggleLanguage();
 			this.currentLang = lang;
+		}
+	}
+
+	toggleMenu() {
+		this.isMenuOpen = !this.isMenuOpen;
+
+		if (this.isMenuOpen) {
+			console.log('+ + + + + Menu open!!!!');
+
+			this.renderer.addClass(document.body, 'no-scroll');
+			document.documentElement.style.setProperty('--scrollbar-thumb', 'var(--bg-color)');
+			document.documentElement.style.setProperty('--scrollbar-thumb-hover', 'var(--bg-color)');
+			document.documentElement.style.setProperty('--scrollbar-cursor', 'default'); // Scrollbar verdunkeln
+		} else {
+			console.log('- - - - - Menu closed!!!!');
+			this.renderer.removeClass(document.body, 'no-scroll');
+			document.documentElement.style.removeProperty('--scrollbar-thumb');
+			document.documentElement.style.removeProperty('--scrollbar-thumb-hover');
+			document.documentElement.style.removeProperty('--scrollbar-cursor'); // Scrollbar zurücksetzen
 		}
 	}
 }
