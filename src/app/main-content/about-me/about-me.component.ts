93da5813b4bd6badc5edf5e9f5eb531bf3eb1e3b
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-03-03 14:12:34 +0100

    Icons

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { HoverImageDirective } from '../../core/directives/hover-image.directive';
 
 @Component({
 	selector: 'app-about-me',
 	standalone: true,
-	imports: [TranslatePipe],
+	imports: [TranslatePipe, HoverImageDirective],
 	templateUrl: './about-me.component.html',
 	styleUrl: './about-me.component.scss',
 })
