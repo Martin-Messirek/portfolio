274b38735722b0044597125b96b67897f3389c47
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-03-05 13:18:14 +0100

    Portfolio 1.0

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
+import { HoverImageDirective } from '../../core/directives/hover-image.directive';
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [TranslatePipe, RouterModule],
+	imports: [TranslatePipe, RouterModule, HoverImageDirective],
 	templateUrl: './footer.component.html',
 	styleUrl: './footer.component.scss',
 })
