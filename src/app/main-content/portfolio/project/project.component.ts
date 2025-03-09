7d86830d5d7090e6104e1c96641ede8c7978a3f5
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-03-09 21:47:12 +0100

    Scroll-animations

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ScrollAnimateDirective } from '../../../core/directives/scroll-animate.directive';
 
 @Component({
 	selector: 'app-project',
 	standalone: true,
-	imports: [CommonModule, TranslatePipe],
+	imports: [CommonModule, TranslatePipe, ScrollAnimateDirective],
 	templateUrl: './project.component.html',
 	styleUrl: './project.component.scss',
 })
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HoverImageDirective } from '../../core/directives/hover-image.directive';
+import { ScrollAnimateDirective } from '../../core/directives/scroll-animate.directive';
 
 @Component({
 	selector: 'app-skills',
 	standalone: true,
-	imports: [TranslatePipe, HoverImageDirective],
+	imports: [TranslatePipe, HoverImageDirective, ScrollAnimateDirective],
 	templateUrl: './skills.component.html',
 	styleUrl: './skills.component.scss',
 })
