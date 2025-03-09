6a50340898acab1024c0bb9285af89a71a34eac8
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-03-09 19:57:38 +0100

    Add scroll animation directive and flipping animation to about-me component

---
+++ src/app/core/directives/scroll-animate.directive.ts
@@ -0,0 +1,47 @@
+import { Directive, ElementRef, Input, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
+
+@Directive({
+	selector: '[appScrollAnimate]',
+	standalone: true,
+})
+export class ScrollAnimateDirective implements AfterViewInit, OnDestroy {
+	@Input('appScrollAnimate') animationClass!: string;
+	// @Input() threshold: number = 0.5; // Standard: 20% Sichtbarkeit nötig
+	options = { rootMargin: '-250px' };
+	private observer!: IntersectionObserver;
+
+	constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+	ngAfterViewInit() {
+		this.observer = new IntersectionObserver(
+			([entry]) => {
+				// console.log('Intersection Ratio 1:', entry.intersectionRatio);
+				// console.log(this.threshold);
+
+				// if (entry.intersectionRatio > 0.5 && entry.intersectionRatio < 1) {
+				// if (entry.intersectionRatio > 0.5) {
+				// if (entry.intersectionRatio > this.options.threshold) {
+				console.log(this.observer);
+
+				if (entry.isIntersecting) {
+					// console.log('Intersection Ratio 2:', entry.intersectionRatio);
+					this.renderer.addClass(this.el.nativeElement, this.animationClass);
+					console.log(this.el.nativeElement, this.animationClass);
+
+					this.observer.disconnect(); // Animation nur einmal auslösen
+				}
+			},
+			this.options,
+			// { threshold: 0.6 },
+			// { threshold: this.threshold },
+		);
+
+		this.observer.observe(this.el.nativeElement);
+	}
+
+	ngOnDestroy() {
+		if (this.observer) {
+			this.observer.disconnect();
+		}
+	}
+}
--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HoverImageDirective } from '../../core/directives/hover-image.directive';
+import { ScrollAnimateDirective } from '../../core/directives/scroll-animate.directive';
 
 @Component({
 	selector: 'app-about-me',
 	standalone: true,
-	imports: [TranslatePipe, HoverImageDirective],
+	imports: [TranslatePipe, HoverImageDirective, ScrollAnimateDirective],
 	templateUrl: './about-me.component.html',
 	styleUrl: './about-me.component.scss',
 })
