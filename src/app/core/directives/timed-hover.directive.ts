fa95dbc39b2941b86e703a3bdaa81f4945170869
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-03-18 09:53:33 +0100

    TimedHoverDirective 2

--- src/app/core/directives/timed-hover.directive.ts
+++ src/app/core/directives/timed-hover.directive.ts
@@ -9,11 +9,14 @@ export class TimedHoverDirective {
 
 	constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-	@HostListener('pointerenter', ['$event'])
-	onPointerEnter(event: PointerEvent) {
-		if (event.pointerType === 'touch') {
-			this.timeout = setTimeout(() => this.removeHoverEffect(), 360);
-		}
+	@HostListener('touchstart', ['$event'])
+	onTouchStart(event: TouchEvent) {
+		this.timeout = setTimeout(() => {
+			this.renderer.removeClass(this.el.nativeElement, 'hover-inactive');
+		}, 1360);
+		// }, 360);
+		// this.timeout = setTimeout(() => this.removeHoverEffect(), 1360); // für Test ''''''''''''''''''''''''''''''
+		// this.timeout = setTimeout(() => this.removeHoverEffect(), 360);
 	}
 
 	@HostListener('contextmenu', ['$event'])
